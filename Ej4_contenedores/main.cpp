b81a8c51d7833a39d5b515429907d4245d86d32a
Suren <146822024+DinamoRoller@users.noreply.github.com>
2023-11-30 16:48:52 +0100

    Add files via upload

---
+++ Ej4_contenedores/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <valarray>
+
+int main() {
+    std::valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    std::cout << "Suma: " << valores.sum() << std::endl;
+    std::cout << "Media: " << valores.sum() / valores.size() << std::endl;
+
+    //suma
+
+    valores += 5.0;
+
+    std::cout << "Valores + 5.0: ";
+    for (double valor : valores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    //resta
+
+    valores -= 2.0;
+
+    std::cout << "Valores - 2.0: ";
+    for (double valor : valores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    //multiplicacion
+
+    valores *= 3.0;
+
+    std::cout << "Valores * 3.0: ";
+    for (double valor : valores){
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    //division
+
+    valores /= 2.0;
+
+    std::cout << "Valores / 2.0: ";
+    for (double valor : valores) {
+        std::cout << valor << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
